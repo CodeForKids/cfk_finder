102dbf781f7d7efce02976986c6fab18647602ed
Josh Chartrand <josh@codeforkids.ca>
2014-11-14 20:03:20 -0500

    Initialize rails app with customer model

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,60 @@
+class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def edit
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+
+    respond_to do |format|
+      if @customer.save
+        format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
+        format.json { render :show, status: :created, location: @customer }
+      else
+        format.html { render :new }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @customer.update(customer_params)
+        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
+        format.json { render :show, status: :ok, location: @customer }
+      else
+        format.html { render :edit }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @customer.destroy
+    respond_to do |format|
+      format.html { redirect_to customers_url, notice: 'Customer was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_customer
+      @customer = Customer.find(params[:id])
+    end
+
+    def customer_params
+      params.require(:customer).permit(:first_name, :last_name, :email)
+    end
+end
