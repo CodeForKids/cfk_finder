e576e6abee8922147097aabea7c592c4a534f4b9
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-11-15 20:22:44 -0500

    More Tests

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -4,18 +4,47 @@ class CustomersControllerTest < ActionController::TestCase
   setup do
     @customer = customers(:one)
     sign_in(@customer.user)
+
+    @customer2 = customers(:two)
+    @no_role_id = users(:no_role_id_customer)
   end
 
-  test "should redirect from new when user has a role" do
+  ########################################
+  # Access to create/new without role id
+  ########################################
+
+  test "should get new when user doesn't has a role id" do
+    sign_in(@no_role_id)
+
+    get :new
+    assert_response :success
+  end
+
+  test "should create customer when user doesn't has a role id" do
+    sign_in(@no_role_id)
+
+    assert_difference('Customer.count') do
+      post :create, customer: { first_name: "John", last_name: "Smith", email: @no_role_id.email }
+    end
+
+    assert_response :redirect
+    assert_redirected_to customer_path(assigns(:customer))
+    assert_equal User.find(@no_role_id.id).role_id, assigns(:customer).id
+  end
+
+  ##################
+  # Has Access Tests
+  ##################
+
+  test "should redirect from new when user has a role id" do
     get :new
     assert_response :redirect
   end
 
-  test "should not create customer when user has a role" do
+  test "should not create customer when user has a role id" do
     assert_no_difference('Customer.count') do
       post :create, customer: { first_name: "", last_name: "Chartrand", email: "josh@example.com" }
     end
-
     assert_redirected_to customer_path(@customer)
   end
 
@@ -57,4 +86,47 @@ class CustomersControllerTest < ActionController::TestCase
 
     assert_redirected_to customers_path
   end
+
+  ########################################
+  # No Access Tests From Another Customer
+  ########################################
+
+  test "should not show customer" do
+    sign_in(@customer2.user)
+
+    get :show, id: @customer
+    assert_response :redirect
+    assert_redirected_to customer_path(@customer2)
+  end
+
+  test "should not get edit for another" do
+    sign_in(@customer2.user)
+
+    get :edit, id: @customer
+    assert_response :redirect
+    assert_redirected_to customer_path(@customer2)
+  end
+
+  test "should not update another customer" do
+    sign_in(@customer2.user)
+
+    patch :update, id: @customer, customer: { first_name: "Josh", last_name: "Chartrand", email: "josh@example.com" }
+    assert_redirected_to customer_path(@customer2)
+
+    customer = Customer.find(@customer.id)
+    assert_equal customer.first_name, @customer.first_name
+    assert_equal customer.last_name, @customer.last_name
+    assert_equal customer.email, @customer.email
+  end
+
+  test "should not destroy another customer" do
+    sign_in(@customer2.user)
+
+    assert_no_difference('Customer.count') do
+      delete :destroy, id: @customer
+    end
+
+    assert_redirected_to customer_path(@customer2)
+  end
+
 end
--- test/controllers/kids_controller_test.rb
+++ test/controllers/kids_controller_test.rb
@@ -4,7 +4,13 @@ class KidsControllerTest < ActionController::TestCase
   setup do
     @kid = kids(:one)
     sign_in(@kid.parent.user)
+
+    @customers2 = customers(:two)
   end
+
+  ##################
+  # Has Access Tests
+  ##################
 
   test "should get new" do
     get :new
@@ -15,7 +21,6 @@ class KidsControllerTest < ActionController::TestCase
     assert_difference('Kid.count') do
       post :create, kid: { date_of_birth: @kid.date_of_birth, first_name: @kid.first_name, gender: @kid.gender, last_name: @kid.last_name }
     end
-
     assert_redirected_to kid_path(assigns(:kid))
   end
 
@@ -23,7 +28,6 @@ class KidsControllerTest < ActionController::TestCase
     assert_no_difference('Kid.count') do
       post :create, kid: { date_of_birth: @kid.date_of_birth, first_name: "", gender: @kid.gender, last_name: @kid.last_name }
     end
-
     assert_template :new
     assert_equal "First name can't be blank", assigns(:kid).errors.full_messages.to_sentence
   end
@@ -54,7 +58,45 @@ class KidsControllerTest < ActionController::TestCase
     assert_difference('Kid.count', -1) do
       delete :destroy, id: @kid
     end
-
     assert_redirected_to kids_path
   end
+
+  ######################
+  # Has No Access Tests
+  ######################
+
+  test "should not show kid for another parent's kid" do
+    sign_in(@customers2.user)
+
+    get :show, id: @kid
+    assert_response :redirect
+    assert_redirected_to root_url
+  end
+
+  test "should not get edit for another parent's kid" do
+    sign_in(@customers2.user)
+
+    get :edit, id: @kid
+    assert_response :redirect
+    assert_redirected_to root_url
+  end
+
+  test "should not update another parent's kid" do
+    sign_in(@customers2.user)
+
+    patch :update, id: @kid, kid: { first_name: "Not_Update" }
+    assert_response :redirect
+    assert_redirected_to root_url
+  end
+
+  test "should not destroy kid for parent's kid" do
+    sign_in(@customers2.user)
+
+    assert_no_difference('Kid.count') do
+      delete :destroy, id: @kid
+    end
+    assert_response :redirect
+    assert_redirected_to root_url
+  end
+
 end
