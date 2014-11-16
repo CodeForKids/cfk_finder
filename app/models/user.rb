2630c4d3ac9b3667e157cc55ad1d8ae8bb89e03c
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-11-16 13:10:20 -0500

    Do not allow changes to user role after creation process

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
   belongs_to :role, :polymorphic => true
+  validates :role_type, inclusion: { in: %w( Parent Tutor ) }
+  before_validation(on: :update) do
+    raise "You cannot change your role" if cannot_change_role?
+  end
 
   def self.roles
     [
@@ -19,5 +21,11 @@ class User < ActiveRecord::Base
 
   def tutor?
     role_type == "Tutor"
+  end
+
+  private
+
+  def cannot_change_role?
+    role_id_was && (role_type_changed? || role_id_changed?)
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:parent)
+  end
+
+  test "should not allow changes to role" do
+    error = assert_raises RuntimeError do
+      @user.role_type = "Tutor"
+      @user.save
+    end
+    assert_equal "You cannot change your role", error.message
+  end
 end
