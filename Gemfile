40bacecdcaabba7068809a7c40654f60904d497d
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-11-17 19:29:11 -0500

    Comment MetaProgrammed Users Controller

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem 'rails', '4.1.7'
 gem 'mysql2'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
-gem 'sdoc', '~> 0.4.0',          group: :doc
+gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'date_validator'
 gem 'devise'
 gem 'geocoder'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
+# This controller is intended to be extended to another controller.
+# - This controller should be the base of all "restricted" users.
+# - This controller will automatically sandbox users to show, edit, update, and destroy.
+# - This controller will allow the user to view new and use create if the user does not have a role_id set
+# - This controller will work with any type of user given that the controller is "Role_Type"Controller.
+# - The subclass controller needs to override resource_params with its own necessities. This is all it needs to do.
+
 class UsersController < ApplicationController
   before_action :authenticate!
   before_action :set_resource, only: [:show, :edit, :update, :destroy]
@@ -11,11 +18,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    # For TutorController, this becomes:
+    # @tutor = Tutor.new
+    # @tutor.address = resource.build_address
     instance_variable_set("@#{controller}", resource_constant.new)
     resource.address = resource.build_address
   end
 
   def create
+    # For TutorController, this becomes:
+    # @tutor = Tutor.new(resource_params)
     instance_variable_set("@#{controller}", resource_constant.new(resource_params))
 
     respond_to do |format|
@@ -65,35 +77,55 @@ class UsersController < ApplicationController
     redirect_to current_user.role unless allowed_view?
   end
 
+  # Essentially if the user is not a "role", redirect to root
+  # Otherwise if the user has a role_id, redirect to their show page
+  # if !current_user.tutor?
+  #   redirect_to root_url
+  # elsif current_user.role_id.present?
+  #   redirect_to current_user.role
+  # end
   def authenticate_creation!
     if !current_user.send("#{controller}?")
       redirect_to root_url
-    elsif !current_user.role_id.nil?
+    elsif current_user.role_id.present?
       redirect_to current_user.role
     end
   end
 
+  # If the controller is TutorsController, this will essentially be
+  # current_user.tutor? && @resource == current_user.role
+  # This requires the user model to implement the role? method
+  # This will essentially check that the user is of type "Role" and is the @resource
   def allowed_view?
     current_user.send("#{controller}?") && @resource == current_user.role
   end
 
+  # If the controller is TutorsController, this will essentially be
+  #   @resource = Tutor.find(params[:id])
+  #   @tutor = @resource
   def set_resource
     @resource = resource_constant.find(params[:id])
     instance_variable_set("@#{controller}", @resource)
   end
 
+  # If the controller is Tutors controller, this will return @tutor
   def resource
     instance_variable_get("@#{controller}".to_sym)
   end
 
+  # This takes the singularized Model name, humanizes it, then changes it to a constant
+  # If the controller is Tutors Controller, this will return Tutor
   def resource_constant
     controller.humanize.constantize
   end
 
+  # For TutorsController (example), this will return tutors
+  # We then singularize this to tutor
   def controller
     params[:controller].singularize
   end
 
+  # To be overriden in the subclass
   def resource_params
     {}
   end
