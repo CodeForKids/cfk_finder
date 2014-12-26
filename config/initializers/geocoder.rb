ee0b9b5fdb0b6650a9a0e30d46badbccf62bfa5e
Julian Nadeau <julian.nadeau@shopify.com>
2014-12-26 15:25:53 -0400

    Dont configure geocode for test env

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,10 +1,11 @@
 require 'autoexpire_cache_dalli'
 require 'dalli'
-
-Geocoder.configure(
- timeout: 5,
- lookup: :telize,
- ip_lookup: :telize,
- units: :km,
- cache: AutoexpireCacheDalli.new(Dalli::Client.new)
-)
+unless Rails.env.test?
+  Geocoder.configure(
+   timeout: 5,
+   lookup: :telize,
+   ip_lookup: :telize,
+   units: :km,
+   cache: AutoexpireCacheDalli.new(Dalli::Client.new)
+  )
+end
